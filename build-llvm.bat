dae4b4953ad70dfc457313c97522cd1a174a5e34
Evan Ovadia <>
2023-04-27 16:39:58 -0600

    Added builder

---
+++ build-llvm.bat
@@ -0,0 +1,29 @@
+echo Downloading LLVM...
+
+mkdir "C:\llvm-tar-xz"
+cd "C:\llvm-tar-xz"
+powershell -c "$ProgressPreference = 'SilentlyContinue' ; Invoke-WebRequest -Uri 'https://github.com/llvm/llvm-project/releases/download/llvmorg-16.0.0/llvm-project-16.0.0.src.tar.xz' -OutFile 'C:\llvm-tar-xz\llvm-project-16.0.0.src.tar.xz'"
+dir "C:\llvm-tar-xz"
+
+echo Unzipping LLVM...
+
+mkdir "C:\llvm-tar"
+cd "C:\llvm-tar"
+7z x "C:\llvm-tar-xz\llvm-project-16.0.0.src.tar.xz" -oC:\llvm-tar
+dir "C:\llvm-tar"
+
+mkdir "C:\llvm-src"
+cd "C:\llvm-src"
+7z x "C:\llvm-tar\llvm-project-16.0.0.src.tar" -oC:\llvm-src
+dir "C:\llvm-src"
+dir "C:\llvm-src\llvm-project-16.0.0.src"
+dir "C:\llvm-src\llvm-project-16.0.0.src\llvm"
+
+echo Building LLVM...
+
+mkdir "C:\llvm-src\llvm-project-16.0.0.src\llvm\build"
+cd "C:\llvm-src\llvm-project-16.0.0.src\llvm\build"
+cmake "C:\llvm-src\llvm-project-16.0.0.src\llvm" -G "Visual Studio 17 2022" -Thost=x64 -A x64 -D "CMAKE_INSTALL_PREFIX=%1" -D CMAKE_BUILD_TYPE=MinSizeRel -D LLVM_TARGETS_TO_BUILD="X86;WebAssembly" -DCMAKE_INSTALL_PREFIX=%1
+dir "C:\llvm-src\llvm-project-16.0.0.src\llvm\build"
+
+tar -xf %1/LLVM.zip -C %1
